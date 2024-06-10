bd4148abf4515abb8c4180f966cff33dd22aa33f
Mikołaj Kozłowski <m.kozlowski1221@gmail.com>
2024-06-10 17:47:33 +0200

    Hangman - Part 2 of Code

--- Hangman/Hangman/Hangman.py
+++ Hangman/Hangman/Hangman.py
@@ -2,19 +2,34 @@ import random
 
 word_list = ["ardvark", "baboon", "camel"]
 
-#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word. CHECKED
+#TODO-1: - Create an empty List called display.
+# For each letter in the chosen_word, add a "_" to 'display'.
+# So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess. Checked
 
-#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase. CHECKED
+#TODO-2: - Loop through each position in the chosen_word;
+# If the letter at that position matches 'guess' then reveal that letter in the display at that position.
+# e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
 
-#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word. CHECKED
+#TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
+# Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
 
+display = []
 chosen_word = random.choice(word_list)
 
+for blank in chosen_word:
+    display.append("_")
+
+print(f"Psst, the soulution is {chosen_word}")
 guess = input("Guess a word: ")
 guess.lower()
 
+index = 0
+
 for word in chosen_word:
     if guess in word:
-        print("Correct")
+        display[index] = guess
     else:
-        print("Wrong")
+        display[index] = "_"
+    index += 1
+
+print(display)
